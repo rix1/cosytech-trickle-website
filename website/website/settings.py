2597874cc6a5401a79b71edde956d631a59746b6
rix1 <rikardeide@gmail.com>
2015-02-06 08:13:55 +0200

    #102 Added top navbar

--- website/website/settings.py
+++ website/website/settings.py
@@ -84,7 +84,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "../static"),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATIC_ROOT = '/var/www/cosytech.net/cosytech-website/static'

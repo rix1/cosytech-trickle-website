974828b80df13f6cf519d7146b83fbbdcc7d31f2
rix1 <rikardeide@gmail.com>
2015-02-11 22:33:03 +0200

    #5 Added extra fields, and bounce animation on hover.

--- website/static/landing/landing.js
+++ website/static/landing/landing.js
@@ -1,33 +1,52 @@
-$('a[href^="#"]').on('click', function(event) {
+$(document).ready(function(){
 
-    var target = $( $(this).attr('href') );
+    animatedHover('#downArrow', 'bounceDown');
 
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: target.offset().top -100
-        }, 500);
-    }
+    // Helper functions
+
+    $('a[href^="#"]').on('click', function(event) {
+
+        var target = $( $(this).attr('href') );
+
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top -350
+            }, 500);
+        }
+    });
+
+
+    // Add animations on hover
+    function animatedHover(element, animation){
+        element = $(element);
+        element.hover(
+            function() {
+                element.addClass('animated ' + animation);
+            },
+            function(){
+                //wait for animation to finish before removing classes
+                window.setTimeout( function(){
+                    element.removeClass('animated ' + animation);
+                }, 2000);
+            }
+        );
+    };
+
+
+    /*
+
+     $(document).ready(function($) {
+     // build scene
+     var scene = new ScrollScene({triggerElement: "#first-section", duration: 300})
+     //var scene = new ScrollScene({duration: 300})
+     .setPin("#first-section")
+     .addTo(controller);
+
+     // show indicators (requires debug extension)
+     scene.addIndicators();
+     });
+     */
+
 
 });
-
-
-var controller;
-$(document).ready(function($) {
-    // init controller
-    controller = new ScrollMagic();
-});
-
-/*
-
-$(document).ready(function($) {
-    // build scene
-    var scene = new ScrollScene({triggerElement: "#first-section", duration: 300})
-    //var scene = new ScrollScene({duration: 300})
-        .setPin("#first-section")
-        .addTo(controller);
-
-    // show indicators (requires debug extension)
-    scene.addIndicators();
-});
-*/

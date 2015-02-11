5066333100d8be92645752e9e356e06a87863357
rix1 <rikardeide@gmail.com>
2015-02-11 10:32:40 +0200

    #5 Fixed responsive info-boxes, and added scroll-down button. Need to animate bounce to increase affordance on that last one.

--- website/static/landing/landing.js
+++ website/static/landing/landing.js
@@ -11,10 +11,23 @@ $('a[href^="#"]').on('click', function(event) {
 
 });
 
-/*
-$("#how").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#why").offset().top
-    }, 2000);
+
+var controller;
+$(document).ready(function($) {
+    // init controller
+    controller = new ScrollMagic();
 });
- */
+
+/*
+
+$(document).ready(function($) {
+    // build scene
+    var scene = new ScrollScene({triggerElement: "#first-section", duration: 300})
+    //var scene = new ScrollScene({duration: 300})
+        .setPin("#first-section")
+        .addTo(controller);
+
+    // show indicators (requires debug extension)
+    scene.addIndicators();
+});
+*/
